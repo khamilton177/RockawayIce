fb7e1fda8e4bc7c3b8196eb03f05bb9f77ed42b2
KHamilton177 <KHamilton177@verizon.net>
2017-06-14 23:41:25 -0400

    fix load error on null var

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -23,36 +23,41 @@ $(document).ready(function(){
     }
   }
 
-  // if (cartL !== null){
     function driveCart(){
-      cartL.classList.add("fadeIn");
-      cartL.style.opacity=1;
-      setTimeout(function(){
-        cart.style.opacity=1;
-        cart.classList.add("bounce");
-      },1000);
+        // Encase attaching eventlistened in if statement to prevent js errors in console
+        if (cartL && cartL != null){
+        cartL.classList.add("fadeIn");
+        cartL.style.opacity=1;
+        setTimeout(function(){
+          cart.style.opacity=1;
+          cart.classList.add("bounce");
+        },1000);
+      }
+
+      cartL.addEventListener("animationend", function(event){
+        cartL.classList.remove("fadeIn");
+      });
+
+      cart.addEventListener("animationend", function(event){
+        cart.classList.remove("bounce");
+        setTimeout(function(){
+          cart.classList.add("lightSpeedOut");
+        },1000);
+      });
+
+      cart.addEventListener("animationend", function(event){
+        setTimeout(function(){
+          cart.classList.remove("lightSpeedOut");
+          cart.style.opacity=0;
+        },7000);
+      });
+
+      window.addEventListener("scroll", function(){
+        var offSet=window.pageYOffset;
+        console.log("Your at "+ offSet);
+        if (offSet > 400){
+          // driveCart();
+        }
+      });
     }
-
-    cartL.addEventListener("animationend", function(event){
-      cartL.classList.remove("fadeIn");
-    });
-
-    cart.addEventListener("animationend", function(event){
-      cart.classList.remove("bounce");
-      setTimeout(function(){
-        cart.classList.add("lightSpeedOut");
-      },1000);
-    });
-
-    cart.addEventListener("animationend", function(event){
-      setTimeout(function(){
-        cart.classList.remove("lightSpeedOut");
-        cart.style.opacity=0;
-      },7000);
-    });
-
-    // setTimeout(function(){
-    //   cartL.style.opacity=0;
-    // },8000);
-  // }
 });
