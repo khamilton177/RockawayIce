a6cab0072443dbec67b2cdd4b642ed3b02499f92
KHamilton177 <kerry.hamilton177@gmail.com>
2017-07-06 23:32:48 -0400

    remove pwd and username validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,8 @@
 class Admin < ApplicationRecord
-  validates :username, uniqueness: true
+  # validates :username, uniqueness: true
   validates :username, presence: true
   validates :email, uniqueness: true
   validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 8 }
+  # validates :password, presence: true, length: { minimum: 8 }
   validates :super, presence: true
 end
