deaf8513792cfbffed5896614caa87d6ff400dd0
KHamilton177 <kerry.hamilton177@gmail.com>
2017-09-14 13:09:42 -0400

    styling for admin portal

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
+//= link_directory ../fonts/roboto .woff
+//= link_directory ../fonts/roboto .woff2
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
